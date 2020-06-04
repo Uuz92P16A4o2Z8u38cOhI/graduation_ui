673edd2dc6bad5d87a7c551e33b5b68fdc49693a
hyl <2546615650@qq.com>
2020-06-04 09:46:42 +0800

    2020/6/4

--- src/utils/global.js
+++ src/utils/global.js
@@ -6,6 +6,7 @@
 
  // 后台管理系统服务器地址
 export const baseUrl = 'http://127.0.0.1:9000/'
+// export const baseUrl = 'http://192.168.91.128:9000/'
 // export const baseUrl = 'https://hyl-java.utools.club/'
 // 文件服务器  （文件+图片）
 export const imgUrl = 'http://192.168.91.128:8888/'
